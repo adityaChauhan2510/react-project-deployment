bacea20cfe86d23931c02cac3b80565d5776446b
Aditya <adityachauhan2501@gmail.com>
2024-06-27 09:45:57 +0530

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      <div className="text-xl underline font-bold text-center border-b">
+        Hello World!!
+      </div>
+    </div>
+  );
+}
+
+export default App;
